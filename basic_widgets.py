bebd67d91498a8f8ac9a908feafeee0bea77c0bd
GLMontanari <gianlucamontanari4@gmail.com>
2020-06-22 19:48:57 +0200

    minimal working example

--- basic_widgets.py
+++ basic_widgets.py
@@ -1,1 +1,19 @@
 #  A simple GUI with the very building blocks ov every GUI
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget
+
+
+class Window(QWidget):
+
+    def __init__(self, parent=None):
+        super(Window, self).__init__(parent)
+
+        self.setWindowTitle("GUI")
+        self.resize(500, 200)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = Window()
+    window.show()
+    app.exec_()
